0da2e418537333a09d5a5534c47e13f6b9e29218
pfu <pfu@sintef.no>
1994-08-23 08:40:42 +0000

    Added handling of kstat<0 from s1301()

--- src/s1303.c
+++ src/s1303.c
@@ -11,7 +11,7 @@
 
 /*
  *
- * $Id: s1303.c,v 1.1 1994-04-21 12:10:42 boh Exp $
+ * $Id: s1303.c,v 1.2 1994-08-23 08:40:42 pfu Exp $
  *
  */
 #define S1303
@@ -19,7 +19,7 @@
 #include "sislP.h"
 
 #if defined(SISLNEEDPROTOTYPES)
-void 
+void
 s1303(double epstrt[],double aepsge,double angle,double epcent[],
 	   double eaxis[],int idim,SISLCurve **rc,int *jstat)
 #else
@@ -35,27 +35,27 @@ void s1303(epstrt,aepsge,angle,epcent,eaxis,idim,rc,jstat)
 #endif
 /*
 *********************************************************************
-*                                                                   
+*
 * PURPOSE    : To create a B-spline approximating a circular arc
 *              around the axis defined by the center point epcent[],
 *              the axis eaxis[] a start point epstrt[] and rotational
 *              angle. The maximal deviation between the true
 *              circular arc and the approximation to the arc
 *              is controlled by aepsge.
-*             
 *
-* INPUT      : epstrt - Start point of circular arc 
-*              aepsge - Maximal deviation allowed between true rotational
-*                       surface and generated surface.
+*
+* INPUT      : epstrt - Start point of circular arc
+*              aepsge - Maximal deviation allowed between true circular arc
+*                       and generated curve.
 *              angle  - The rotational angle. Counter clockwise around axis.
 *                       If the rotational angle is outside <-PI,+PI> then
 *                       a closed curve is produced.
-*              epcent - SISLPoint of axis of circle  
+*              epcent - SISLPoint of axis of circle
 *              eaxis  - Normal vector to plane of circle
 *              idim   - The dimension of the space (=3)
 *
-* OUTPUT     : 
-*              jstat  - status messages  
+* OUTPUT     :
+*              jstat  - status messages
 *                                         > 0      : warning
 *                                         = 0      : ok
 *                                         < 0      : error
@@ -81,9 +81,9 @@ void s1303(epstrt,aepsge,angle,epcent,eaxis,idim,rc,jstat)
 *
 * REFERENCES :
 *
-*-                                      
+*-
 * CALLS      : s6norm, s6scpr, s1301,s6rotax, s6mvec, s6err
-*              
+*
 *
 * WRITTEN BY : Tor Dokken, SI, Oslo, Norway. 24. May 1988
 * REVISED BY : J. Kaasa, SI, Aug. 92 (Made a proper handling of 2D circles).
@@ -102,116 +102,122 @@ void s1303(epstrt,aepsge,angle,epcent,eaxis,idim,rc,jstat)
   int    kstat;           /* Status variable                            */
   double tfak;            /* Value of cross product                     */
   int    kpos = 1;        /* Possition of error                         */
-  
+
   double tdum;            /* Intermediate storing facility              */
   int    klimit;          /* Limit in for loop                          */
-  
-  
+
+
   /* The routine is only working for dimension 2 & 3 */
-  
-  if (idim != 2 && idim != 3) goto err104;		
-			      
+
+  if (idim != 2 && idim != 3) goto err104;
+
   if (idim == 2)
   {
      /* Make difference between start point and point on axis */
-     
+
      s6diff(epstrt, epcent, idim, sdiff);
 
      /* Find length of this vector, which is the radius of the circle */
-     
+
      tlength = s6length(sdiff,idim,&kstat);
   }
-  
+
   else if (idim == 3)
   {
      /* Normalize axis direction */
-     
+
      (void)s6norm(eaxis,idim,saxis,&kstat);
-  
+
      /* Find distance between axis and start point of circle */
-  
+
      /* Make difference between start point and point on axis */
-     
+
      s6diff(epstrt, epcent, idim, sdiff);
      tfak = s6scpr(sdiff,saxis,idim);
-  
+
      /* Find vector normal to axis going to vertex by subtracing the
      component of the difference vector along the axis */
-     
+
      for (kj=0;kj<3;kj++)
        {
          sdiff[kj] = sdiff[kj] - tfak*saxis[kj];
        }
-  
+
      /* Find length of this vector, which is the radius of the circle */
-     
+
      tlength = s6length(sdiff,idim,&kstat);
   }
-     
+
   /* Calculate relative error. If relative error  <= 0, the whole curve
   lies on the axis  */
-  
+
   if (tlength <= (double)0.0) goto err127;
   treler = aepsge/tlength;
-  
-  
+
+
   /* Calculate normalized circle */
-  
+
   s1301(treler,angle,idim,rc,&kstat);
-  
+  if (kstat < 0)  goto error;
+
   /* Make local variables for curve description */
-  
+
   scoef1 = (*rc) -> ecoef;
   kn1    = (*rc) -> in;
-  
-  
+
+
   if (idim == 2)
   {
-     
+
      /* Transform the circle approx into the right position. */
-     
+
      klimit = idim*kn1;
      for (kj = 0; kj < klimit; kj += 2)
      {
 	tdum = epcent[0] + scoef1[kj]*sdiff[0] - scoef1[kj + 1]*sdiff[1];
-	scoef1[kj + 1] = epcent[1] + scoef1[kj + 1]*sdiff[0] 
+	scoef1[kj + 1] = epcent[1] + scoef1[kj + 1]*sdiff[0]
 	   + scoef1[kj]*sdiff[1];
 	scoef1[kj] = tdum;
      }
   }
-  
+
   else if (idim == 3)
   {
-	
+
      /* Transform the circle approx into the right position, first make
      transformation matrix */
-  
+
      s6rotax(epcent,saxis,epstrt,smat,&kstat);
-  
+
      /* Transform the vertices into right position */
-  
+
      s6mvec(smat,scoef1,kn1,scoef1);
   }
-  
+
   *jstat = 0;
   goto out;
-  
-  
+
+
   /* Error in input, dimension not equal to 3 */
-  
- err104: 
+
+ err104:
   *jstat = -104;
   s6err("s1303",*jstat,kpos);
   goto out;
-  
+
   /* Error in input, point lies on axis */
-  
- err127: 
+
+ err127:
   *jstat = -127;
   s6err("s1303",*jstat,kpos);
   goto out;
-  
+
+  /* Error in lower level routine. */
+
+ error:
+  *jstat = kstat;
+  goto out;
+
  out:
   return;
 }
-
