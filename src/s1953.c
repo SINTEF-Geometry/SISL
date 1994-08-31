023dce027c8ca1610490a4378eb05f1cc6367884
pfu <pfu@sintef.no>
1994-08-31 10:36:35 +0000

    Added correct handling of periodicity (making input k-regular first).

--- src/s1953.c
+++ src/s1953.c
@@ -11,7 +11,7 @@
 
 /*
  *
- * $Id: s1953.c,v 1.1 1994-04-21 12:10:42 boh Exp $
+ * $Id: s1953.c,v 1.2 1994-08-31 10:36:35 pfu Exp $
  *
  */
 
@@ -21,7 +21,7 @@
 #include "sislP.h"
 
 #if defined(SISLNEEDPROTOTYPES)
-void 
+void
 s1953(SISLCurve *pcurve,double epoint[],int idim,double aepsco,double aepsge,
 	   int *jpt,double **gpar,int *jcrv,SISLIntcurve ***wcurve,int *jstat)
 #else
@@ -41,7 +41,7 @@ void s1953(pcurve,epoint,idim,aepsco,aepsge,jpt,gpar,jcrv,wcurve,jstat)
 *********************************************************************
 *
 *********************************************************************
-*                                                                   
+*
 * PURPOSE    : Find the closest point between the curve pcurve and the
 *              point epoint.
 *
@@ -57,8 +57,8 @@ void s1953(pcurve,epoint,idim,aepsco,aepsge,jpt,gpar,jcrv,wcurve,jstat)
 *
 * OUTPUT     : jpt    - Number of single closest points.
 *              gpar   - Array containing the parameter values of the
-*                       single closest points in the parameter interval 
-*                       of the curve. The points lie continuous. 
+*                       single closest points in the parameter interval
+*                       of the curve. The points lie continuous.
 *                       Closest curves are stored in wcurve.
 *              jcrv   - Number of closest curves.
 *              wcurve - Array containing descriptions of the closest
@@ -66,7 +66,7 @@ void s1953(pcurve,epoint,idim,aepsco,aepsge,jpt,gpar,jcrv,wcurve,jstat)
 *                       in the parameter interval. The curve-pointers points
 *                       to nothing. (See descrjption of Intcurve
 *                       in intcurve.dcl).
-*              jstat  - status messages  
+*              jstat  - status messages
 *                                         > 0      : warning
 *                                         = 0      : ok
 *                                         < 0      : error
@@ -76,81 +76,116 @@ void s1953(pcurve,epoint,idim,aepsco,aepsge,jpt,gpar,jcrv,wcurve,jstat)
 *              circel/sphere with center epoint and radius zero. The
 *              result is a curve of dimension one. Find the minimum
 *              points of this curve.
-*              
+*
 *
 *
 * REFERENCES :
 *
-*- 
+*-
 * CALLS      : s1321,s1370,s1920,freeCurve.
 *
 * WRITTEN BY : Vibeke Skytt, SI, 88-11.
+* Revised by : Paal Fugelli, SINTEF, Oslo, Norway, 1994-08.  Updated to handle
+*              input curve with periodic basis correctly.
 *
 *********************************************************************
-*/                                                               
-{                                                                     
+*/
+{
   int kstat = 0;            /* Local status variable.                    */
   int kpos = 0;             /* Position of error.                        */
   int kdim = 1;             /* Dimension of curve in extremal problem.   */
   double tradius = 0;       /* Radius of circel/sphere describing point. */
   double tdir = -1;         /* Direction of extremal value.              */
   double sarray[16];        /* Matrix describing circel/sphere.          */
-  SISLCurve *qc = NULL;         /* Curve of which to find extremal points.   */
-  int ratflag = 0;         /* Flag if curve is rational. */
-  
+  SISLCurve *qc = NULL;     /* Curve of which to find extremal points.   */
+  SISLCurve *qkreg = NULL;  /* Input curve with ensured k-regular basis. */
+  int ratflag = 0;          /* Flag to indicate if curve is rational.    */
+  int ki;                   /* Counter.                                  */
+
+
+  /* Ensure input has k-regular knot basis. */
+
+  if ( pcurve -> cuopen == SISL_CRV_PERIODIC )
+  {
+    /* Cyclic (periodic) curve */
+
+    make_cv_kreg(pcurve, &qkreg, &kstat);
+    if ( kstat < 0 )  goto error;
+  }
+  else
+    qkreg = pcurve;
+
+
   /* Test input.  */
-  
-  if (idim != 2 && idim != 3) goto err105;
-  if (pcurve->idim != idim) goto err106;
-  
-  if(pcurve->ikind == 2 || pcurve->ikind == 4) ratflag=1;
-  
+
+  if ( idim != 2 && idim != 3 )  goto err105;
+  if ( qkreg -> idim != idim )  goto err106;
+
+  if ( qkreg -> ikind == 2 || qkreg -> ikind == 4 )  ratflag = 1;
+
   /* Make a matrix of dimension (idim+1)*(idim+1) to describe
      the circel/shpere.                                        */
-  
-  s1321(epoint,tradius,idim,kdim,sarray,&kstat);
-  if (kstat < 0) goto error;
-  
+
+  s1321(epoint, tradius, idim, kdim, sarray, &kstat);
+  if ( kstat < 0 )  goto error;
+
   /* Put curve into circel/sphere equation.  */
-  
-  s1370(pcurve,sarray,idim,kdim,ratflag,&qc,&kstat);
-  if (kstat < 0) goto error;
-  
+
+  s1370(qkreg, sarray, idim, kdim, ratflag, &qc, &kstat);
+  if ( kstat < 0 )  goto error;
+
   /* Find minimum points of the new curve.  */
-  
-  s1920(qc,&tdir,kdim,aepsco,aepsge,jpt,gpar,jcrv,wcurve,&kstat);
-  if (kstat < 0) goto error;
-  
+
+  s1920(qc, &tdir, kdim, aepsco, aepsge, jpt, gpar, jcrv, wcurve, &kstat);
+  if ( kstat < 0 )  goto error;
+
+  /* Handle periodicity (remove extraneous points) */
+  if ( *jpt > 1  &&  idim > 1  &&  pcurve -> cuopen == SISL_CRV_PERIODIC )
+  {
+    for ( ki=0;  ki < (*jpt);  ki++ )
+    {
+      if ( (*gpar)[ki] == pcurve -> et[pcurve->in] )
+      {
+	(*jpt)--;
+	(*gpar)[ki] = (*gpar)[*jpt];
+	ki--;
+      }
+    }
+  }
+
   /* Closest points/intervals found.  */
-  
+
   *jstat = 0;
   goto out;
-  
+
+
   /* Error in input. Dimension not equal to 2 or 3.  */
-  
- err105: *jstat = -105;
+
+ err105:
+  *jstat = -105;
   s6err("s1953",*jstat,kpos);
   goto out;
-  
+
   /* Dimensions conflicting.  */
-  
- err106: *jstat = -106;
+
+ err106:
+  *jstat = -106;
   s6err("s1953",*jstat,kpos);
   goto out;
-  
+
   /* Error in lower level routine.  */
-  
-  error : *jstat = kstat;
+
+ error:
+  *jstat = kstat;
   s6err("s1953",*jstat,kpos);
   goto out;
-  
+
  out:
-  
+
   /* Free allocated space.  */
-  
-  if (qc != NULL) freeCurve(qc);
-  
+
+  if ( qkreg  &&  qkreg != pcurve )  freeCurve(qkreg);
+  if (qc)  freeCurve(qc);
+
   return;
-}                                               
-                                           
-                       
+}
