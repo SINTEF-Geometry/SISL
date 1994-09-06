797da30b05ffbd29507833fd8a7caed8f1e8403d
pfu <pfu@sintef.no>
1994-09-06 11:50:02 +0000

    Fixed memory leak by moving the free'ing of 'qkreg' from outfree to out.

--- src/s1730.c
+++ src/s1730.c
@@ -11,7 +11,7 @@
 
 /*
  *
- * $Id: s1730.c,v 1.1 1994-04-21 12:10:42 boh Exp $
+ * $Id: s1730.c,v 1.2 1994-09-06 11:50:02 pfu Exp $
  *
  */
 
@@ -21,7 +21,7 @@
 #include "sislP.h"
 
 #if defined(SISLNEEDPROTOTYPES)
-void 
+void
 s1730(SISLCurve *pc,SISLCurve **rcnew,int *jstat)
 #else
 void s1730(pc,rcnew,jstat)
@@ -66,6 +66,8 @@ void s1730(pc,rcnew,jstat)
 *
 * WRITTEN BY : Arne Laksaa, SI, 88-11.
 * REVISED BY : Johannes Kaasa, SI, May 1992 (Inroduced NURBS)
+* Revised by : Paal Fugelli, SINTEF, Oslo, Norway, Sept. 1994. Moved free'ing
+*              of 'qkreg' from label 'outfree' to label 'out'.
 *
 **********************************************************************/
 {
@@ -86,22 +88,22 @@ void s1730(pc,rcnew,jstat)
   double *scoef=NULL;    /* The new vertice.                           */
   SISLCurve *qkreg = NULL;   /* Input curve made k-regular.            */
   SISLCurve *q1=NULL;    /* Pointer to new curve-object.               */
-  
+
   double *rcoef;         /* Potential rational coefficients.           */
-  
+
   /* Check that we have a curve to treat. */
-  
+
   if (!pc) goto err150;
-  
+
   if (pc->cuopen == SISL_CRV_PERIODIC)
   {
      make_cv_kreg(pc,&qkreg,&kstat);
      if (kstat < 0) goto err153;
   }
   else qkreg = pc;
-  
+
   /* Check if this is a rational curve. */
-  
+
   if (qkreg->ikind == 2 || qkreg->ikind == 4)
     {
        kdim++;
@@ -109,46 +111,46 @@ void s1730(pc,rcnew,jstat)
     }
   else
     rcoef = qkreg->ecoef;
-  
+
   /* Allocate space for the kk elements which may not be zero in eache
      line of the basic transformation matrix, and space for new knots
      to use in s1701.c */
-  
+
   if ((salfa=newarray(kk,double))==NULL) goto err101;
   if ((sp=newarray(kk,double))==NULL) goto err101;
-  
+
   /* Find the number of vertices in the new curve. */
-  
+
   for(ki=0,kn1=0;ki<kn+kk;ki+=kj,kn1+=kk)
     for(kj=1;ki+kj<kn+kk && (qkreg->et[ki] == qkreg->et[ki+kj]);kj++);
   kn1 -= kk;
-  
+
   /* Allocating the new arrays to the new curve. */
-  
+
   if ((st=newarray(kn1+kk,double))==NULL) goto err101;
   if ((scoef=new0array(kn1*kdim,double))==NULL) goto err101;
-  
+
   /* Making the new knotvectors. */
-  
+
   for(ki=0,ki1=0;ki<kn+kk;ki+=kj)
     {
       for(kj=1;ki+kj<kn+kk && (qkreg->et[ki] == qkreg->et[ki+kj]);kj++);
       for(kj1=0;kj1<kk;kj1++,ki1++) st[ki1] = qkreg->et[ki];
     }
-  
+
   /* Updating the coefisientvector to the new curve.*/
-  
+
   for(s1=scoef,ki=0,kmy=0;ki<kn1;ki++)
     {
       /* Here we compute a new line with line number ki of
 	 the knot inserten matrix. */
-      
+
       while(qkreg->et[kmy+1] <= st[ki]) kmy++;
       s1701(ki,kmy,kk,kn,&kpl,&kfi,&kla,st,qkreg->et,sp,salfa,&kstat);
       if (kstat) goto err153;
-      
+
       /* Compute the kdim vertices with the same "index". */
-      
+
       for (kj=0; kj<kdim; kj++,s1++)
 	for (*s1=0,kj1=kfi,kj2=kfi+kpl; kj1<=kla; kj1++,kj2++)
 	  {
@@ -156,53 +158,52 @@ void s1730(pc,rcnew,jstat)
 	    *s1 += salfa[kj2] * rcoef[ki2];
 	  }
     }
-  
+
   /* Allocating new curve-objects.*/
-  
+
   if ((q1=newCurve(kn1,kk,st,scoef,qkreg->ikind,qkreg->idim,2)) == NULL) goto err101;
-  
+
   /* Updating output. */
-  
+
   *rcnew = q1;
   *jstat = 0;
   goto out;
-  
-  
+
+
   /* Error. Subrutine error. */
-  
+
  err153: *jstat = kstat;
   goto outfree;
-  
-  
+
+
   /* Error. No curve to treat.  */
-  
+
  err150: *jstat = -150;
   s6err("s1730",*jstat,kpos);
   goto out;
-  
-  
-  
+
+
+
   /* Error. Allocation error, not enough memory.  */
-  
+
  err101: *jstat = -101;
   s6err("s1730",*jstat,kpos);
   goto outfree;
-  
-  
+
+
  outfree:
-  if (qkreg != NULL && qkreg != pc) freeCurve(qkreg);
   if(q1) freeCurve(q1);
   else
     {
       if (st) freearray(st);
       if (scoef) freearray(scoef);
     }
-  
-  
+
+
   /* Free local used memory. */
-  
- out: if (salfa) freearray(salfa);
+
+ out:
+  if (qkreg != NULL && qkreg != pc) freeCurve(qkreg);
+  if (salfa) freearray(salfa);
   if (sp) freearray(sp);
 }
-
-
