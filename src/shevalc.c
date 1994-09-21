c901808261f82bdcce375b12a2ea718f05d3043b
pfu <pfu@sintef.no>
1994-09-21 09:06:22 +0000

    Did fix from VSK (consequence of having changed s1991() by not doing smoothing anymore) - caused error in memory accessing in call to newCurve().

--- src/shevalc.c
+++ src/shevalc.c
@@ -11,7 +11,7 @@
 
 /*
  *
- * $Id: shevalc.c,v 1.1 1994-04-21 12:10:42 boh Exp $
+ * $Id: shevalc.c,v 1.2 1994-09-21 09:06:22 pfu Exp $
  *
  */
 
@@ -21,7 +21,7 @@
 #include "sislP.h"
 
 #if defined(SISLNEEDPROTOTYPES)
-void 
+void
 shevalc(SISLCurve *pc1,int ider,double ax,double aepsge,int *ileft,
 	     double eder[],int *jstat)
 #else
@@ -38,7 +38,7 @@ void shevalc(pc1,ider,ax,aepsge,ileft,eder,jstat)
 *********************************************************************
 *
 *********************************************************************
-*                                                                   
+*
 * PURPOSE    : To compute the value and ider first derivatives of the
 *              B-spline curve pointed to by pc1, at the point with
 *              parameter value ax. Use the filtered coefficients of the
@@ -57,14 +57,14 @@ void shevalc(pc1,ider,ax,aepsge,ileft,eder,jstat)
 *                       position and derivatives.
 *              aepsge - Geometry resolution.
 *
-*                
+*
 *
 * INPUT/OUTPUT : ileft - Pointer to the interval in the knot vector
 *                        where ax is located. If et is the knot vector,
 *                        the relation
-*                          
+*
 *                          et[ileft] <= ax < et[ileft+1]
-* 
+*
 *                        should hold. (If ax == et[in] then ileft should
 *                        be in-1. Here in is the number of B-spline
 *                        coefficients.)
@@ -86,13 +86,13 @@ void shevalc(pc1,ider,ax,aepsge,ileft,eder,jstat)
 *                       vector, and so on.
 *                       (The C declaration of eder as a two dimensional array
 *                       would therefore be eder[ider+1,idim].)
-*              jstat  - Status messages  
+*              jstat  - Status messages
 *                                         > 0      : Warning.
 *                                         = 0      : Ok.
 *                                         < 0      : Error.
 *
 *
-* METHOD     : 
+* METHOD     :
 *
 * REFERENCES :
 *
@@ -100,27 +100,49 @@ void shevalc(pc1,ider,ax,aepsge,ileft,eder,jstat)
 * CALLS      : s1221    - Evaluate curve.
 *              s1991    - Make the direction cone of a curve.
 *              newCurve - Create new curve object.
-*              freeCurve - Free scratch occupied by curve object. 
+*              freeCurve - Free scratch occupied by curve object.
 *
 * WRITTEN BY :  Vibeke Skytt, SI, 04.91.
 * CORRECTED BY: UJK, SI, 06.91
+* Modified by : Paal Fugelli, SINTEF, Oslo, Norway, 09.94. Replaced
+*               code using 'pdir->esmooth' and s1991() with 'qc = pc1;'
+*               and modified the free'ing of 'qc'.
+*               Added check for 1D rationals.
 *********************************************************************
-*/                                     
+*/
 {
   int kstat=0;        /* Local status variable.                          */
   int kdim = pc1->idim;  /* Dimension of geometry space.                 */
   double *scoef=NULL;    /* Array storing filtered coefficients.         */
   double *s1,*s2,*s3,*s4; /* Pointers into coefficient arrays.           */
   SISLCurve *qc = NULL;   /* Curve to evaluate.                          */
-  
+
   /* Make sure that the filtered coefficients of the curve exist.  */
-  
+
   if (kdim == 1)
   {
+
+    /*
+     * PFU 09-94.
+     * There should never be a rational 1D curve here according to UJK, but
+     * I (PFU) added a test just in case...
+     * A rational curve would have caused a memory usage error in newCurve
+     * when trying to divide out the weights from the coefs.
+     * This could result in a core dump (division by zero) since the data
+     * would be "garbage".
+     *
+     * If future changes requires this to handle rational 1D curves, this
+     * must be updated to use rcoef when input is rational.
+     *
+     */
+
+    if ( pc1->ikind == 2 || pc1->ikind == 4 )
+      goto err151;
+
      /* Create filtered coefficients. */
-     
+
      if ((scoef = newarray(pc1->in,DOUBLE)) == NULL) goto err101;
-     
+
      for (s1=pc1->ecoef, s2=scoef, s3=s1+pc1->in; s1<s3; s1=s4)
      {
 	*s2 = *s1;
@@ -130,53 +152,53 @@ void shevalc(pc1,ider,ax,aepsge,ileft,eder,jstat)
 	   else break;
 	}
      }
-     
+
      /* Create curve object.  */
-     
+
      if ((qc = newCurve(pc1->in,pc1->ik,pc1->et,scoef,pc1->ikind,
 			kdim,0)) == NULL) goto err101;
   }
   else
-  {
-     /* Check if the cone of the curve exist. */
-     
-     if (pc1->pdir == NULL)
-     {
-	/* Create cone.  */
-	
-	s1991(pc1,aepsge,&kstat);
-	if (kstat < 0) goto error;
-     }
-     
-     /* Create curve object.  */
-     
-     if ((qc = newCurve(pc1->in,pc1->ik,pc1->et,pc1->pdir->esmooth,
-			pc1->ikind,kdim,0)) == NULL) goto err101;
-  }
-  
+    qc = pc1;
+
+  /*
+   * This previously used AN ANACRONISM ('pdir->esmooth') - taken out
+   * (Confirmed by VSK).
+   */
+
+
+
   /* Evaluate curve.  */
-  
+
   s1221(qc,ider,ax,ileft,eder,&kstat);
   if (kstat < 0) goto error;
-	
+
   /* UJK Let's have a normal exit possibility !*/
   *jstat = 0;
   goto out;
 
-  /* Error in scratch allocation.  */
-  err101: *jstat = -101;
-  goto out;
-  
-  /* Error in lower level routine.  */
-  
-  error : *jstat = kstat;
+
+  /* Error in input (1D rationals is not handled) */
+ err151:
+  *jstat = -151;
   goto out;
 
-out: 
+  /* Error in scratch allocation.  */
+ err101:
+  *jstat = -101;
+  goto out;
+
+  /* Error in lower level routine.  */
+
+ error:
+  *jstat = kstat;
+  goto out;
+
+out:
    /* Free scratch occupied by local objects. */
-   
+
    if (scoef != NULL) freearray(scoef);
-   if (qc != NULL) freeCurve(qc);
-   
+   if (qc != NULL && qc != pc1 ) freeCurve(qc);
+
    return;
 }
