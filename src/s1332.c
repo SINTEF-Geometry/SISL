275c0e58c4f850f517903d0065885f3ba5a45772
pfu <pfu@sintef.no>
1994-10-13 15:54:06 +0000

    Fixed memory leaks and ensured returned surface had valid value

--- src/s1332.c
+++ src/s1332.c
@@ -11,7 +11,7 @@
 
 /*
  *
- * $Id: s1332.c,v 1.1 1994-04-21 12:10:42 boh Exp $
+ * $Id: s1332.c,v 1.2 1994-10-13 15:54:06 pfu Exp $
  *
  */
 
@@ -21,7 +21,7 @@
 #include "sislP.h"
 
 #if defined(SISLNEEDPROTOTYPES)
-void 
+void
 s1332(SISLCurve *pc1,SISLCurve *pc2,double aepsge,double ep[],SISLSurf **rs,int *jstat)
 #else
 void s1332(pc1,pc2,aepsge,ep,rs,jstat)
@@ -34,7 +34,7 @@ void s1332(pc1,pc2,aepsge,ep,rs,jstat)
 #endif
 /*
 *********************************************************************
-*                                                                   
+*
 * PURPOSE    : To create a swept B-spline surface by making
 *              the tensor-product of two B-spline curves.
 *
@@ -49,9 +49,9 @@ void s1332(pc1,pc2,aepsge,ep,rs,jstat)
 *                       the point as contact point. If the point is not lying
 *                       on any of the curve, then the surface will not
 *                       interpolate any of the curves.
-*	
 *
-* OUTPUT     : jstat  - status messages  
+*
+* OUTPUT     : jstat  - status messages
 *                        > 0      : warning
 *                        = 0      : ok
 *                        < 0      : error
@@ -62,11 +62,12 @@ void s1332(pc1,pc2,aepsge,ep,rs,jstat)
 *                       in1 in2
 *              P(u,v) = SUM SUM ((p1(i) + p2(j)-ep)*w1(i)*w2(j)*B(i,ik1)*B(j,ik2)
 *                       i=1 j=1
-*-                                      
+*-
 * CALLS      : s1707,s6err.
 *
 * WRITTEN BY : A. M. Ytrehus  SI,  Oslo, Norway.  Sep. 1988
-* REVISED BY : Johannes Kaasa SI,  Sep. 1991 (Introduced NURBS) 
+* REVISED BY : Johannes Kaasa SI,  Sep. 1991 (Introduced NURBS)
+* Revised by : Paal Fugelli, SINTEF, Oslo, Norway, Oct. 1994. Removed memory leaks.
 *********************************************************************
 */
 {
@@ -94,41 +95,44 @@ void s1332(pc1,pc2,aepsge,ep,rs,jstat)
   double *weight1 = NULL; /* Rational weights in first direction        */
   double *weight2 = NULL; /* Rational weights in second direction       */
   double weight;          /* Tensor product weights                     */
-  
+
+
+  *rs = NULL;
+
   /* The curves must have the same dimension      */
   if (pc1 -> idim != pc2 -> idim) goto err106;
-  
+
   /* Make local pointers to description of curves */
-  
+
   if (!pc1) goto err150;
   if (!pc2) goto err150;
-  
+
   s1707(pc1,&kstat);
   if (kstat<0) goto error;
-  
+
   s1707(pc2,&kstat);
   if (kstat<0) goto error;
-  
+
   sknot1 = pc1 -> et;
   scoef1 = pc1 -> ecoef;
   rcoef1 = pc1 -> rcoef;
   kn1 = pc1 -> in;
   kord1 = pc1 -> ik;
   kdim = pc1 -> idim;
-  
+
   sknot2 = pc2 -> et;
   scoef2 = pc2 -> ecoef;
   rcoef2 = pc2 -> rcoef;
   kn2 = pc2 -> in;
   kord2 = pc2 -> ik;
-  
+
   /* Allocate vertex-array for the surface. */
 
   rdim = kdim + 1;
   if (pc1->ikind == 2 || pc1->ikind == 4
-                      || pc2->ikind == 2 || pc2->ikind == 4) 
+                      || pc2->ikind == 2 || pc2->ikind == 4)
     scoef = newarray(kn1*kn2*rdim,DOUBLE);
-  else 
+  else
     scoef = newarray(kn1*kn2*kdim,DOUBLE);
   if (!scoef) goto err101;
 
@@ -150,9 +154,9 @@ void s1332(pc1,pc2,aepsge,ep,rs,jstat)
   else
     for (ki=0; ki<kn2; ki++)
       weight2[ki] = 1.;
-  
+
   /* Compute the vertices of the surface. */
-  
+
   sp = scoef;
   for (kj=0; kj<kn2; kj++)
     {
@@ -162,10 +166,10 @@ void s1332(pc1,pc2,aepsge,ep,rs,jstat)
 	  spc2 = scoef2 + kj*kdim;
 	  spnt = ep;
           weight = weight1[ki]*weight2[kj];
-	  
+
 	  for (kp=0; kp<kdim; kp++)
             {
-	      *sp = (*spc1 + *spc2 - *spnt)*weight; 
+	      *sp = (*spc1 + *spc2 - *spnt)*weight;
 	      sp++;
 	      spc1++;
 	      spc2++;
@@ -176,12 +180,12 @@ void s1332(pc1,pc2,aepsge,ep,rs,jstat)
             {
               *sp = weight;
               sp++;
-            }  
+            }
         }
     }
-  
+
   /* Create the surface */
-  
+
   kcopy = 1;
   if (pc1->ikind == 2 || pc1->ikind == 4
                       || pc2->ikind == 2 || pc2->ikind == 4)
@@ -189,29 +193,35 @@ void s1332(pc1,pc2,aepsge,ep,rs,jstat)
   else
     kind = 1;
   *rs =  newSurf(kn1,kn2,kord1,kord2,sknot1,sknot2,scoef,kind,kdim,kcopy);
-  
+
   *jstat = 0;
   goto out;
-  
+
   /* Empty curve. */
  err150: *jstat = -150;
   s6err("s1332",*jstat,kpos);
   goto out;
-  
+
   /* Different dimension for curves   */
  err106: *jstat = -106;
   s6err("s1332",*jstat,kpos);
   goto out;
-  
+
   /* Error in space allocation. */
  err101: *jstat = -101;
   s6err("s1332",*jstat,kpos);
   goto out;
-  
+
   /* Error in lower level routine. */
  error:  *jstat = kstat;
   s6err("s1332",*jstat,kpos);
   goto out;
  out:
+
+  /* Free local memory */
+  if (scoef) freearray(scoef);
+  if (weight1) freearray(weight1);
+  if (weight2) freearray(weight2);
+
   return;
 }
