1d274fa3450b7d08b48252f014b1852223e39e28
pfu <pfu@sintef.no>
1994-11-17 09:00:55 +0000

    Fixed memory leaks and changed interface from 'double*' to 'double[]' for 'center'.

--- src/s1015.c
+++ src/s1015.c
@@ -11,7 +11,7 @@
 
 /*
  *
- * $Id: s1015.c,v 1.1 1994-04-21 12:10:42 boh Exp $
+ * $Id: s1015.c,v 1.2 1994-11-17 09:00:55 pfu Exp $
  *
  */
 #define S1015
@@ -39,7 +39,7 @@ static void c_c_f_s9dir();
 void
       s1015(SISLCurve *pc1,SISLCurve *pc2,double aepsge,
 	   double eps1[],double eps2[],double aradius,double *parpt1,
-	   double *parpt2,double *center,int *jstat)
+	   double *parpt2,double center[],int *jstat)
 #else
 void s1015(pc1,pc2,aepsge,eps1,eps2,aradius,parpt1,parpt2,
            center,jstat)
@@ -50,23 +50,23 @@ void s1015(pc1,pc2,aepsge,eps1,eps2,aradius,parpt1,parpt2,
      double eps2[];
      double aradius;
      double *parpt1;
-     double *parpt2;
-     double *center;
+     double parpt2[];
+     double center[];
      int    *jstat;
 #endif
 /*
 *********************************************************************
-*                                                                   
+*
 * PURPOSE    : To iterate to the start and end point of a fillet between
-*              two curves. The center of the (circular) fillet is also
+*              two 2D curves. The center of the (circular) fillet is also
 *              calculated.
 *
-* INPUT      : pc1    - The first input curve.   
-*              pc2    - The second input curve.   
-*              aepsge - Geometry resolution.         
-*              eps1   - Point telling that the fillet should be put on 
+* INPUT      : pc1    - The first 2D input curve.
+*              pc2    - The second 2D input curve.
+*              aepsge - Geometry resolution.
+*              eps1   - 2D point telling that the fillet should be put on
 *                       the side of curve 1 where eps1 is situated.
-*              eps2   - Point telling that the fillet should be put on the 
+*              eps2   - 2D point telling that the fillet should be put on the
 *                       side of curve 2 where eps2 is situated.
 *              aradius - The radius to be used on the fillet.
 *
@@ -76,33 +76,39 @@ void s1015(pc1,pc2,aepsge,eps1,eps2,aradius,parpt1,parpt2,
 *              parpt2 - Parameter value of the point on curve 2 where the
 *                       fillet ends. Input is a guess value for the iteration.
 *
-* OUTPUT     : center - Center of the (circular) fillet.
+* OUTPUT     : center - 2D center of the (circular) fillet.  Space must be
+*                       allocated outside the function.
 *              status - Status:
 *                              = 1      : converged
 *                              = 2      : diverged
 *                              < 0      : error
-*                        
-* METHOD     : 
 *
-* USE        : 
-*             
+* METHOD     :
+*
+* USE        :
+*
 * REFERENCES :
-*                   
-*-                                                 
-* CALLS      : 
+*
+*-
+* CALLS      :
 *
 *
 * WRITTEN BY : Johannes Kaasa, SI, April 1992.
-*              
+* Revised by : Paal Fugelli, SINTEF, Oslo, Norway, Nov. 1994.  Clearified
+*              header and changed the function interface declaration of 'center'
+*              - must now be allocated outside the function (was memory overwrite).
+*              Initialized 'p1', 'p2' and 'kstat' and fixed memory leak from
+*              'p1' and 'p2'.
+*
 *********************************************************************
 */
 {
-  
-  int kstat;          /* Status variable                                  */
+
+  int kstat=0;        /* Status variable                                  */
   int kpos=0;         /* Position of error                                */
 
-  SISLPoint *p1;      /* SISLPoint form of eps1.                          */
-  SISLPoint *p2;      /* SISLPoint form of eps2.                          */
+  SISLPoint *p1=NULL; /* SISLPoint form of eps1.                          */
+  SISLPoint *p2=NULL; /* SISLPoint form of eps2.                          */
   double tpar1;       /* Test parameter on curve 1.                       */
   double tpar2;       /* Test parameter on curve 2.                       */
   int kder = 1;       /* Number of derivatives.                           */
@@ -126,136 +132,136 @@ void s1015(pc1,pc2,aepsge,eps1,eps2,aradius,parpt1,parpt2,
   double tdn[2];      /* Distances between old and new parameter
 			 value in the two parameter directions.           */
   double tprev;       /* Previous difference between the curves.          */
-  double *sval1=NULL; /* Value ,first and second derivatie on curve 1     */ 
-  double *sval2;      /* Value ,first and second derivatie on curve 2     */ 
+  double *sval1=NULL; /* Value ,first and second derivatie on curve 1     */
+  double *sval2;      /* Value ,first and second derivatie on curve 2     */
   double *sdiff;      /* Difference between the curves                    */
   double start1;      /* Start parameter of curve 1.                      */
   double start2;      /* Start parameter of curve 2.                      */
   double end1;        /* End parameter of curve 1.                        */
   double end2;        /* End parameter of curve 2.                        */
-  
+
   /* Check dimensions */
-  
+
   if (pc1->idim != 2 || pc2->idim != 2) goto err105;
-  
+
   start1 = pc1->et[pc1->ik - 1];
   start2 = pc2->et[pc2->ik - 1];
   end1 = pc1->et[pc1->in];
   end2 = pc2->et[pc2->in];
-  
+
   /* Calculate the offset radius of curve 1. */
-  
+
   if (!(p1 = newPoint(eps1, 2, 0))) goto err101;
   s1771(p1, pc1, REL_COMP_RES, start1, end1, *parpt1,
 	&tpar1, &kstat);
   if (kstat < 0) goto error;
 
-  s1221(pc1, kder, tpar1, &kleft1, derive, &kstat); 
+  s1221(pc1, kder, tpar1, &kleft1, derive, &kstat);
   if (kstat < 0) goto error;
-  
-  tdot = (eps1[0] - derive[0])*(-derive[3]) 
+
+  tdot = (eps1[0] - derive[0])*(-derive[3])
                               + (eps1[1] - derive[1])*derive[2];
   if (tdot > (double)0.)
     rad1 = aradius;
   else
     rad1 = -aradius;
-  
+
   /* Calculate the offset radius of curve 2. */
-  
+
   if (!(p2 = newPoint(eps2, 2, 0))) goto err101;
   s1771(p2, pc2, REL_COMP_RES, start2, end2, *parpt2,
 	&tpar2, &kstat);
   if (kstat < 0) goto error;
 
-  s1221(pc2, kder, tpar2, &kleft2, derive, &kstat); 
+  s1221(pc2, kder, tpar2, &kleft2, derive, &kstat);
   if (kstat < 0) goto error;
-  
-  tdot = (eps2[0] - derive[0])*(-derive[3]) 
+
+  tdot = (eps2[0] - derive[0])*(-derive[3])
                               + (eps2[1] - derive[1])*derive[2];
   if (tdot > (double)0.)
     rad2 = aradius;
   else
     rad2 = -aradius;
-  
+
   /* Fetch endpoints and the intervals of parameter interval of curves.  */
-  
+
   tdelta1 = end1 - start1;
   tdelta2 = end2 - start2;
-  
+
   /* Allocate local used memory */
-  
+
   sval1 = newarray((2*kder+5)*kdim,double);
   if (sval1 == NULL) goto err101;
-  
+
   sval2 = sval1 + (kder+2)*kdim;
   sdiff = sval2 + (kder+2)*kdim;
-  
+
   /* Initiate variables.  */
-  
+
   tprev = (double)HUGE;
-  
+
   /* Evaluate 0-1.st derivatives of both curves */
-  
+
   ev_cv_off(pc1, kder, *parpt1, &kleft1, rad1, sval1, &kstat);
   if (kstat < 0) goto error;
-  
+
   ev_cv_off(pc2, kder, *parpt2, &kleft2, rad2, sval2, &kstat);
   if (kstat < 0) goto error;
-  
+
   /* Compute the distanse vector and value and the new step. */
-  
+
   c_c_f_s9dir(&tdist,td,td+1,sdiff,sval1,sval2,kdim);
-  
+
   /* Correct if we are not inside the parameter intervall. */
-  
+
   t1[0] = td[0];
   t1[1] = td[1];
   c_c_f_s9corr(t1,*parpt1,*parpt2,start1,end1,start2,end2);
-  
+
   /* Iterate to find the intersection point.  */
-  
+
   for (knbit = 0; knbit < 50; knbit++)
     {
       /* Evaluate 0-1.st derivatives of both curves */
-      
+
        ev_cv_off(pc1, kder, *parpt1+t1[0], &kleft1, rad1, sval1, &kstat);
       if (kstat < 0) goto error;
-      
+
       ev_cv_off(pc2, kder, *parpt2+t1[1], &kleft2, rad2, sval2, &kstat);
       if (kstat < 0) goto error;
-      
+
       /* Compute the distanse vector and value and the new step. */
-      
+
       c_c_f_s9dir(&tdist,tdn,tdn+1,sdiff,sval1,sval2,kdim);
-      
+
       /* Check if the direction of the step have change. */
-      
+
       kdir = (s6scpr(td,tdn,2) >= DNULL);     /* 0 if changed. */
-      
+
       /* Ordinary converging. */
-      
+
       if (tdist < tprev*(double)0.9 || kdir)
 	{
 	  *parpt1 += t1[0];
 	  *parpt2 += t1[1];
-	  
+
           td[0] = tdn[0];
           td[1] = tdn[1];
-	  
+
 	  /* Correct if we are not inside the parameter intervall. */
-	  
+
 	  t1[0] = td[0];
 	  t1[1] = td[1];
 	  c_c_f_s9corr(t1,*parpt1,*parpt2,start1,end1,start2,end2);
-	  
+
           if ( (fabs(t1[0]/tdelta1) <= REL_COMP_RES) &&
 	      (fabs(t1[1]/tdelta2) <= REL_COMP_RES) ) break;
-	  
+
           tprev = tdist;
 	}
-      
+
       /* Not converging, corrigate and try again. */
-      
+
       else
 	{
           t1[0] /= (double)2;
@@ -263,44 +269,47 @@ void s1015(pc1,pc2,aepsge,eps1,eps2,aradius,parpt1,parpt2,
           knbit--;
 	}
     }
-  
+
   /* Calculate the centerpoint. */
-  
+
   center[0] = (sval1[0] + sval2[0])/2.;
   center[1] = (sval1[1] + sval2[1])/2.;
-  
+
   /* Iteration stopped, test if point founds found is within resolution */
-  
+
   if (tdist <= aepsge)
     *jstat = 1;
   else
     *jstat = 2;
-  
+
   /* Iteration completed.  */
-  
-  
+
+
   goto out;
-  
+
   /* Error in allocation */
-  
+
  err101: *jstat = -101;
   s6err("s1015",*jstat,kpos);
-  goto out;                  
-  
+  goto out;
+
   /* Dimension nmot equal to 2 */
-  
- err105: 
+
+ err105:
   *jstat = -105;
   s6err("s1015",*jstat,kpos);
   goto out;
-  
+
   /* Error in lower level routine.  */
-  
+
   error : *jstat = kstat;
   s6err("s1015",*jstat,kpos);
-  goto out;                  
-  
- out:    if (sval1 != NULL) freearray(sval1);
+  goto out;
+
+ out:
+  if (sval1 != NULL) freearray(sval1);
+  if ( p1 != NULL )  freePoint(p1);
+  if ( p2 != NULL )  freePoint(p2);
 }
 
 #if defined(SISLNEEDPROTOTYPES)
@@ -322,7 +331,7 @@ static void c_c_f_s9corr(gdn,acoef1,acoef2,astart1,aend1,astart2,aend2)
 *********************************************************************
 *
 *********************************************************************
-*                                                                   
+*
 * PURPOSE    : To be sure that we are inside the boorder of the
 *              parameter plan. If we are outside clipping is used
 *	       to corrigate the step value.
@@ -340,7 +349,7 @@ static void c_c_f_s9corr(gdn,acoef1,acoef2,astart1,aend1,astart2,aend2)
 * INPUT/OUTPUT : gdn   - Old and new step value.
 *
 *
-* METHOD     : 
+* METHOD     :
 *
 *
 * REFERENCES :
@@ -349,16 +358,16 @@ static void c_c_f_s9corr(gdn,acoef1,acoef2,astart1,aend1,astart2,aend2)
 * WRITTEN BY : Arne Laksaa, SI, Feb 1989
 *
 *********************************************************************
-*/                       
+*/
 {
-  if (acoef1 + gdn[0] < astart1) 
+  if (acoef1 + gdn[0] < astart1)
     gdn[0] = astart1 - acoef1;
-  else if (acoef1 + gdn[0] > aend1)   
+  else if (acoef1 + gdn[0] > aend1)
     gdn[0] = aend1 - acoef1;
 
-  if (acoef2 + gdn[1] < astart2) 
+  if (acoef2 + gdn[1] < astart2)
     gdn[1] = astart2 - acoef2;
-  else if (acoef2 + gdn[1] > aend2)   
+  else if (acoef2 + gdn[1] > aend2)
     gdn[1] = aend2 - acoef2;
 }
 
@@ -381,7 +390,7 @@ static void c_c_f_s9dir(cdist,cdiff1,cdiff2,gdiff,eval1,eval2,idim)
 *********************************************************************
 *
 *********************************************************************
-*                                                                   
+*
 * PURPOSE    : To compute the distance vector and value beetween
 *	       a points on the first curve and a point on the second
 *	       curve. And to compute a next step on both curves.
@@ -396,9 +405,9 @@ static void c_c_f_s9dir(cdist,cdiff1,cdiff2,gdiff,eval1,eval2,idim)
 *
 *
 * OUTPUT     : gdiff   - Array to use when computing the differens vector.
-*	       cdiff1  - Relative parameter value in intersection 
+*	       cdiff1  - Relative parameter value in intersection
 *                        point in first direction.
-*              cdiff2  - Relative parameter value in intersection 
+*              cdiff2  - Relative parameter value in intersection
 *                        point in second direction.
 *              cdist   - The value to the point in the distance surface.
 *
@@ -409,7 +418,7 @@ static void c_c_f_s9dir(cdist,cdiff1,cdiff2,gdiff,eval1,eval2,idim)
 *	       to both tangents. If the distance vector beetween the two
 *	       points on the curve is "diff" and the two derevativ vectors
 *	       are "der1" and "der2", and the two wanted parameter distance
-*	       are "dt1" and "dt2", then we get the following system of 
+*	       are "dt1" and "dt2", then we get the following system of
 *	       equations:
 *		 <dt1*der1+dist-dt2*der2,der2> = 0
 *		 <dt1*der1+dist-dt2*der2,der1> = 0
@@ -429,41 +438,40 @@ static void c_c_f_s9dir(cdist,cdiff1,cdiff2,gdiff,eval1,eval2,idim)
 * WRITTEN BY : Arne Laksaa, SI, Feb 1989
 *
 *********************************************************************
-*/                       
-{                        
+*/
+{
   int kstat;		           /* Local status variable. */
   register double tdet;		   /* Determinant */
   register double t1,t2,t3,t4,t5;  /* Variables in equation system */
-  
+
   /* Computing the different vector */
-  
+
   s6diff(eval1,eval2,idim,gdiff);
-  
+
   /* Computing the length of the different vector. */
-  
+
   *cdist = s6length(gdiff,idim,&kstat);
-  
+
   t1 =  s6scpr(eval1+idim,eval1+idim,idim);
   t2 =  s6scpr(eval1+idim,eval2+idim,idim);
   t3 =  s6scpr(eval2+idim,eval2+idim,idim);
   t4 =  s6scpr(gdiff,eval1+idim,idim);
   t5 =  s6scpr(gdiff,eval2+idim,idim);
-  
+
   /* Computing the determinant. */
-  
+
   tdet = t2*t2 - t1*t3;
-  
+
   if (DEQUAL(tdet,DNULL))
     {
       *cdiff1 = DNULL;
       *cdiff2 = DNULL;
     }
-  else 
+  else
     {
       /* Using Cramer's rule to find the solution of the system. */
-      
+
       *cdiff1 =  (t4*t3 - t5*t2)/tdet;
       *cdiff2 =  (t2*t4 - t1*t5)/tdet;
     }
 }
-
