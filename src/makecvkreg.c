8798993bfb5a61064890c87384fcd5aaa498a5ff
pfu <pfu@sintef.no>
1994-11-30 14:37:16 +0000

    *** empty log message ***

--- src/makecvkreg.c
+++ src/makecvkreg.c
@@ -11,7 +11,7 @@
 
 /*
  *
- * $Id: makecvkreg.c,v 1.6 1994-11-30 12:51:50 pfu Exp $
+ * $Id: makecvkreg.c,v 1.7 1994-11-30 14:37:16 pfu Exp $
  *
  */
 
@@ -60,6 +60,8 @@ void
 *
 * WRITTEN BY : Ulf J. Krystad, SI, 04.92.
 * Revised by : Paal Fugelli, SINTEF, Oslo, Norway, 94-08. Added error propagation.
+* Revised by : Paal Fugelli, SINTEF, Oslo, Norway, Nov.1994. Set cuopen flag to
+*              closed when changed from periodic.
 **********************************************************************/
 {
    int kn=pc->in;	/* Number of vertices in 1. par. dir.  */
@@ -68,6 +70,9 @@ void
    /* Pick part of curve */
    s1712 (pc, pc->et[kk-1], pc->et[kn], rcnew, jstat);
   if (*jstat < 0)  goto error;
+
+   if (pc->cuopen == SISL_CRV_PERIODIC )
+     (*rcnew)->cuopen = SISL_CRV_CLOSED;
 
   goto out;
 
