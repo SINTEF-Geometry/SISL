6d248800cb3f5b4099bd6fa25fa4d33701cdbf29
pfu <pfu@sintef.no>
1995-01-26 10:28:50 +0000

    Made the deallocation of 'wsurf' safer.

--- src/s1860.c
+++ src/s1860.c
@@ -11,7 +11,7 @@
 
 /*
  *
- * $Id: s1860.c,v 1.1 1994-04-21 12:10:42 boh Exp $
+ * $Id: s1860.c,v 1.2 1995-01-26 10:28:50 pfu Exp $
  *
  */
 
@@ -40,7 +40,7 @@ void s1860(ps,eview,idim,aepsco,aepsge,jpt,gpar,jcrv,wcurve,jstat)
 *********************************************************************
 *
 *********************************************************************
-*                                                                   
+*
 * PURPOSE    : Find the silhouette curves and points of a surface when
 *              the surface is viewed from a specific direction. In
 *              addition to the points and curves found by this routine,
@@ -59,7 +59,7 @@ void s1860(ps,eview,idim,aepsco,aepsge,jpt,gpar,jcrv,wcurve,jstat)
 * OUTPUT     : jpt    - Number of single silhouette points.
 *              gpar   - Array containing the parameter values of the
 *                       single silhouette points in the parameter
-*                       plane of the surface. The points lie continuous. 
+*                       plane of the surface. The points lie continuous.
 *                       Silhouette curves are stored in wcurve.
 *              jcrv   - Number of silhouette curves.
 *              wcurve - Array containing descriptions of the silhouette
@@ -67,13 +67,13 @@ void s1860(ps,eview,idim,aepsco,aepsge,jpt,gpar,jcrv,wcurve,jstat)
 *                       in the parameter plane. The curve-pointers points
 *                       to nothing. (See description of Intcurve
 *                       in intcurve.dcl).
-*              jstat  - status messages  
+*              jstat  - status messages
 *                                         > 0      : warning
 *                                         = 0      : ok
 *                                         < 0      : error
 *
 *
-* METHOD     : 
+* METHOD     :
 *
 *
 * REFERENCES : Main routine written by Vibeke Skytt, SI, 1988.
@@ -81,10 +81,12 @@ void s1860(ps,eview,idim,aepsco,aepsge,jpt,gpar,jcrv,wcurve,jstat)
 * CALLS      : sh1860, s6err.
 *
 * WRITTEN BY : Christophe Rene Birkeland, SINTEF, 93-06.
+* Revised by : Paal Fugelli, SINTEF, Oslo, Norway, Jan. 1995.
+*              Made deallocation of 'wsurf' safer.
 *
 *********************************************************************
 */
-{            
+{
   int kstat = 0;              /* Local status variable.                      */
   int kpos = 0;               /* Position of error.                          */
   int i;
@@ -101,23 +103,26 @@ void s1860(ps,eview,idim,aepsco,aepsge,jpt,gpar,jcrv,wcurve,jstat)
 
   if(pretop != NULL) freearray(pretop);
 
-  for(i=0; i<jsurf; i++)
-    freeIntsurf(wsurf[i]);
-  if(wsurf != NULL) freearray(wsurf);
+  if ( wsurf != NULL )
+  {
+    for(i=0; i<jsurf; i++)
+      freeIntsurf(wsurf[i]);
+    freearray(wsurf);
+  }
 
-  if(jsurf > 0) 
+  if(jsurf > 0)
     *jstat=10;
-  else 
+  else
     *jstat = 0;
-  goto out;   
+  goto out;
 
   /* Error in lower level routine.  */
 
-  error : 
+  error :
     *jstat = kstat;
     s6err("s1860",*jstat,kpos);
     goto out;
 
   out:
     return;
-}                                               
+}
