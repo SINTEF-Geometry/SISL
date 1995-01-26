a72c8b98c3393f88a3c38c8333f3bac3fb0b642c
pfu <pfu@sintef.no>
1995-01-26 09:02:02 +0000

    Changed the order of the initialization of 'wsurf' and 'jsurf' to ensure that they are initialized before a 'goto' exit.

--- src/hp_s1880.c
+++ src/hp_s1880.c
@@ -11,7 +11,7 @@
 
 /*
  *
- * $Id: hp_s1880.c,v 1.1 1994-04-21 12:10:42 boh Exp $
+ * $Id: hp_s1880.c,v 1.2 1995-01-26 09:02:02 pfu Exp $
  *
  */
 
@@ -25,11 +25,11 @@ void
     hp_s1880(SISLObject * po1, SISLObject * po2,
 	     int ideg,
 	     int ipar1, int ipar2, SISLIntdat *pintdat,
-	     int *jpar, double **gpar1, double **gpar2, int **pretop, 
-	     int *jcrv, SISLIntcurve *** wcrv, int *jsurf, SISLIntsurf *** wsurf, 
+	     int *jpar, double **gpar1, double **gpar2, int **pretop,
+	     int *jcrv, SISLIntcurve *** wcrv, int *jsurf, SISLIntsurf *** wsurf,
 	     int *jstat)
 #else
-void 
+void
    hp_s1880(po1, po2, ideg, ipar1, ipar2, pintdat, jpar, gpar1, gpar2, pretop, jcrv,
        wcrv, jsurf, wsurf, jstat)
      SISLObject *po1;
@@ -129,19 +129,19 @@ void
   int kdir;
   int exact=FALSE, exact_treat=FALSE;
   int log_test = 0;
-  double dummy;	
+  double dummy;
   /* ------------------------------------------------------------------ */
-  
+
   for (ki = 1; ki < 5; ki++)
      log_test |= 1 << ki;
-  
+
   if (po1->iobj == SISLSURFACE && ideg != 0)
   {
      /* Surf vs Implicit geometry */
      qo2 = newObject (SISLPOINT);
      exact_treat = TRUE;
   }
-  else  if (po1->iobj == SISLSURFACE && 
+  else  if (po1->iobj == SISLSURFACE &&
 	    po2->iobj == SISLSURFACE &&
 	    ideg ==0)
   {
@@ -151,18 +151,20 @@ void
   }
 
 
-  /* Initiate output arrays.  */
+  /* Initiate output.  */
 
   *gpar1 = *gpar2 = NULL;
   *wcrv = NULL;
+  *wsurf = NULL;
+
+  *jcrv = 0;
+  *jsurf = 0;
 
   /* Allocate space for intersection curve and surface array.  */
 
-  *jcrv = 0;
   *wcrv = newarray (jlist, SISLIntcurve *);
   if (jlist > 0 && *wcrv == NULL)
     goto err101;
-  *jsurf = 0;
   *wsurf = newarray (jlist, SISLIntsurf *);
   if (jlist > 0 && *wcrv == NULL)
     goto err101;
@@ -180,49 +182,49 @@ void
      kpoint = (*vlist)->inumb;
      if (kpoint == 0)
 	goto err137;
-     
+
      if (qpfirst->iinter == SI_TRIM && qpfirst == qplast)
      {
 	/* Create new intersection surf.  */
-	
+
 	*usurf = newIntsurf(*vlist);
 	if (*usurf == NULL)
 	   goto err101;
-	
-	/* Copy pretopology 
+
+	/* Copy pretopology
 	   memcopy((*usurf)->pretop,(*vlist)->pretop,4,int); */
-	
-	kpt += kpoint-1;	      
+
+	kpt += kpoint-1;
 	usurf++;
 	(*jsurf)++;
      }
      else
      {
-	if (qpfirst->iinter == SI_SING || 
+	if (qpfirst->iinter == SI_SING ||
 	    (sh6nmbmain (qpfirst,&kstat)) > 2)
 	   sing_1 = TRUE;
 	else
 	   sing_1 = FALSE;
-	
-	if (qplast->iinter == SI_SING || 
+
+	if (qplast->iinter == SI_SING ||
 	    (sh6nmbmain (qplast,&kstat)) > 2)
 	   sing_2 = TRUE;
 	else
 	   sing_2 = FALSE;
-	
-	
+
+
 	/* Allocate space for arrays containing parameter values of points
 	   in intersection curves.                                          */
-	
+
 	spar1 = newarray (ipar1 * kpoint, double);
 	spar2 = newarray (ipar2 * kpoint, double);
 	if ((ipar1 > 0 && spar1 == NULL) ||
 	    (ipar2 > 0 && spar2 == NULL))
 	   goto err101;
-	
+
 	/* Collect parameter values of the points in this intersection list
 	   and distribute values to the objects in the intersection.         */
-	
+
 	kj = 0;
 	stpar1 = spar1;
 	stpar2 = spar2;
@@ -233,12 +235,12 @@ void
 	      *(stpar1++) = *(stpar3++);
 	   for (kk = 0; kk < ipar2; kk++)
 	      *(stpar2++) = *(stpar3++);
-	   
+
 	   /* Reduce no of single points */
 	   if (qpt->marker != -99)
 	   {
 	      kpt++;
-	      
+
 	      /* Flag point */
 	      qpt->marker = -99;
 	   }
@@ -255,9 +257,9 @@ void
 	   }
 	   kj++;
 	}
-	
+
 	/* Find type of intersection curve.  */
-	
+
 	if (sing_1 && sing_2)
 	   /* Both ends junction */
 	   ktype = 7;
@@ -273,17 +275,17 @@ void
 	else
 	   /* Open and clean */
 	   ktype = 4;
-	
-	
+
+
 	exact = FALSE;
-	
+
 	/* UJK, January 1993, if exact curve mark it with type 9. */
 	if (exact_treat &&
 	    kj == 2 &&
 	    (qpfirst->curve_dir[(*vlist)->ind_first] & log_test))
 	{
-	   
-	   
+
+
 	   /* Constant parameter curve */
 	   for (kdir = 0; kdir < qpfirst->ipar; kdir++)
 	      if (qpfirst->curve_dir[(*vlist)->ind_first] &
@@ -295,27 +297,27 @@ void
 	      }
 	}
 
-	
+
 	/* Create new intersection curve.  */
 	*ucrv = newIntcurve (kj, ipar1, ipar2, spar1, spar2, ktype);
 	if (*ucrv == NULL)
 	   goto err101;
-	
+
 	/* Copy pretopology */
 	memcopy((*ucrv)->pretop,(*vlist)->pretop,4,int);
-	
-	
+
+
 	/* UJK, January 1993, if exact curve mark it with type 9. */
 	if (exact)
 	{
-	   
-	   pick_crv_sf (po1, qo2, kdir, qpfirst, 
+
+	   pick_crv_sf (po1, qo2, kdir, qpfirst,
 			qplast, &(*ucrv)->pgeom, &kstat);
 	   if (kstat < 0)
 	      goto error;
-	   
+
 	   /* UJK, Pick 2D line  */
-	   
+
 	   if (kdir >= po1->iobj)
 	      s1602(&(qpfirst->epar[po1->iobj]),
 		    &(qplast->epar[po1->iobj]),
@@ -325,7 +327,7 @@ void
 		    &dummy,
 		    &(*ucrv)->ppar2,
 		    &kstat);
-	   
+
 	   else
 	      s1602(qpfirst->epar,
 		    qplast->epar,
@@ -335,23 +337,23 @@ void
 		    &dummy,
 		    &(*ucrv)->ppar1,
 		    &kstat);
-	   
+
 	   if (kstat < 0) goto error;
-	   
+
 	}
-	
-	
+
+
 	ucrv++;
 	(*jcrv)++;
      }
      vlist++;
   }
-  
+
   /* Find number of single intersection points.  */
 
   kpt = jpt - kpt;
   if (kpt < 0) goto err137;
-	     
+
   /* Create arrays to keep parameter values of intersection points.  */
 
   *gpar1 = newarray (ipar1 * kpt, double);
@@ -481,9 +483,8 @@ error:
   goto out;
 
 
-out:    
+out:
    if (po1->iobj == SISLSURFACE && ideg != 0)
       freeObject (qo2);
 return;
 }
-
