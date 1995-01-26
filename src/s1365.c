80404b3f9aa623916ec1c6c6b66f65b314c2f9c6
pfu <pfu@sintef.no>
1995-01-26 15:42:05 +0000

    Updated the header (it has handled NURBS input for a long time!)

--- src/s1365.c
+++ src/s1365.c
@@ -11,7 +11,7 @@
 
 /*
  *
- * $Id: s1365.c,v 1.2 1994-06-03 08:20:35 vsk Exp $
+ * $Id: s1365.c,v 1.3 1995-01-26 15:42:05 pfu Exp $
  *
  */
 
@@ -37,13 +37,13 @@ void s1365(ps,aoffset,aepsge,amax,idim,rs,jstat)
 *********************************************************************
 *
 *********************************************************************
-*                                                                   
+*
 * PURPOSE    : To create a B-spline approximating the offset surface of
-*              a B-spline surface.
+*              a NURBS surface.
 *
 *
 *
-* INPUT      : ps     - The input B-spline surface.
+* INPUT      : ps     - The input NURBS surface.
 *              aoffset- The offset distance.
 *                       If idim=2 a positive signe on this value put the
 *                       offset on the side of the positive normal vector,
@@ -59,8 +59,8 @@ void s1365(ps,aoffset,aepsge,amax,idim,rs,jstat)
 *                       box side is used.
 *              idim   - The dimension of the space (2 or 3).
 *
-* OUTPUT     : 
-*              jstat  - status messages  
+* OUTPUT     :
+*              jstat  - status messages
 *                                         > 0      : warning
 *                                         = 0      : ok
 *                                         < 0      : error
@@ -69,7 +69,7 @@ void s1365(ps,aoffset,aepsge,amax,idim,rs,jstat)
 * METHOD     : The 4 edge curves of the surface are extracted. Offset curves
 *              of these 4 edge curves are approximated and a common
 *              basis for the two pairs of opposite offset curves is calculated.
-*              Vertices are recomputed. 
+*              Vertices are recomputed.
 *
 * EXAMPLE OF USE:
 *
@@ -79,7 +79,7 @@ void s1365(ps,aoffset,aepsge,amax,idim,rs,jstat)
 *
 *-
 * CALLS      : s1435     - Pick a given edge-curve of a B-spline surface.
-*              s1360     - Approximate the offset curve of a B-spline curve.
+*              s1360     - Approximate the offset curve of a NURBS curve.
 *              s1933     - Put a set of curves on a common basis.
 *              s1366     - Create a B-spline surface approximating the offset
 *                          surface of a B-spline surface
@@ -99,94 +99,94 @@ void s1365(ps,aoffset,aepsge,amax,idim,rs,jstat)
   int knbcrv = 2;    /* Number of curves in set. */
   int kopen = 1;     /* Flag telling that the resulting surface should
 			be open in both parameter directions. */
-  int kn13,kord13;   /* Number of vertices and order of edge curves along  
+  int kn13,kord13;   /* Number of vertices and order of edge curves along
 			1. parameter direction. */
-  int kn24,kord24;   /* Number of vertices and order of edge curves along  
+  int kn24,kord24;   /* Number of vertices and order of edge curves along
 			2. parameter direction. */
   int nder[4];       /* Number of edges along each surface edge.         */
   double sp[4];      /* Parameter value of edge in constatnt direction.  */
   double toffset = (double)0.0; /* Local offset value for extraction of edge-
 				   curves. */
   double snorm[3];   /* Local normal vector for extraction of edge-curves. */
-  double tstart1,tend1; /* Endpoints of parameter interval in first 
+  double tstart1,tend1; /* Endpoints of parameter interval in first
 			   direction.                                     */
-  double tstart2,tend2; /* Endpoints of parameter interval in second 
+  double tstart2,tend2; /* Endpoints of parameter interval in second
 			   direction.                                     */
   double *sknot13=NULL;/* Pointer to common knot-vector of edge curves along
 			  1. parameter direction. */
   double *sknot24=NULL;/* Pointer to common knot-vector of edge curves along
 			  2. parameter direction. */
   int  kk;              /* Loop controller. */
-  
+
   /* Initialization of variables */
   kdim = ps -> idim;
-  for (kk=0; kk<4; kk++)  
+  for (kk=0; kk<4; kk++)
   {
      nder[kk] = 1;
      pc[kk] = NULL;
      rc[kk] = NULL;
   }
   for (kk=0; kk<3; kk++) snorm[kk] = DNULL;
-  
+
   /* Fetch the 4 edge-curves of surface */
-  
+
   for (kk=0; kk<4; kk++)
     {
       s1435(ps,kk,&pc[kk],&sp[kk],&kstat);
       if (kstat < 0) goto error;
     }
-  
+
   /* Create a B-spline curve approximating the offset curve of the 4 edges */
-  
+
   for (kk=0; kk<4; kk++)
     {
       s1360(pc[kk],toffset,aepsge,snorm,amax,kdim,&rc[kk],&kstat);
       if (kstat<0) goto error;
     }
-  
+
   /* Rearrange the pointers to the 4 edge curves. */
-  
+
   rc13[0] = rc[0];
   rc13[1] = rc[2];
   rc24[0] = rc[1];
   rc24[1] = rc[3];
-  
+
   /* Fetch endpoints of parameter intervals.  */
-  
+
   tstart1 = *(ps->et1 + ps->ik1 - 1);
   tend1 = *(ps->et1 + ps->in1);
   tstart2 = *(ps->et2 + ps->ik2 - 1);
   tend2 = *(ps->et2 + ps->in2);
-  
+
   /* Put the edge curves along 1. parameter direction into common basis. */
 
   s1933(knbcrv,rc13,tstart1,tend1,&sknot13,&kn13,&kord13,&kstat);
   if (kstat<0) goto error;
-  
+
   /* Put the edge curves along 2. parameter direction into common basis. */
-  
+
   s1933(knbcrv,rc24,tstart2,tend2,&sknot24,&kn24,&kord24,&kstat);
   if (kstat<0) goto error;
-  
+
   /* Create a B-spline surface approximating the offset surface of a B-spline
      surface. */
-  
+
   s1366(ps,aoffset,aepsge,amax,idim,sknot13,kn13,kord13,
 	sknot24,kn24,kord24,rs,&kstat);
   if (kstat<0) goto error;
-  
+
   /* Surface approximated. */
-  
+
   *jstat = 0;
   goto out;
-  
+
   /* Error in lower level routine.  */
-  
+
   error : *jstat = kstat;
   s6err("s1365",*jstat,kpos);
   goto out;
-  
-  out: 
+
+  out:
      for (kk=0; kk<4; kk++)
      {
 	if (pc[kk] != NULL) freeCurve(pc[kk]);
@@ -194,6 +194,6 @@ void s1365(ps,aoffset,aepsge,amax,idim,rs,jstat)
      }
      if (sknot13 != NULL) freearray(sknot13);
      if (sknot24 != NULL) freearray(sknot24);
-     
+
      return;
 }
