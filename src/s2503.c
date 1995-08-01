e5ff63324a8c0a887ab836c26bba6ec5de9884aa
jka <jka@sintef.no>
1995-08-01 09:03:23 +0000

    no entry

--- src/s2503.c
+++ src/s2503.c
@@ -11,7 +11,7 @@
 
 /*
  *
- * $Id: s2503.c,v 1.7 1995-06-28 11:06:50 jka Exp $
+ * $Id: s2503.c,v 1.8 1995-08-01 09:03:23 jka Exp $
  *
  */
 
@@ -90,41 +90,29 @@ void s2503(SISLSurf *surf, int ider, double derive[], double normal[],
 *
 *
 * WRITTEN BY :  Geir Westgaard, SINTEF, Oslo, Norway.             Date: 1995-1
-* CORRECTED BY :  Ulf J Krystad, SINTEF, Oslo, Norway.             Date: 1995-1
+* CORRECTED BY :  Ulf J Krystad, SINTEF, Oslo, Norway.            Date: 1995-1
 *                 Removed knot navigators + some clean up.
-* CORRECTED BY :  Johannes Kaasa, SINTEF, Oslo, Norway.           Date: 1995-06
-*                 Error in explicit curvature.
+* CORRECTED BY :  Johannes Kaasa, SINTEF, Oslo, Norway.           Date: 1995-8
+*                 Calculated the fundamental form coefficients by
+*                 calls to s2513.
 ******************************************************************************
 */
 {
-  double a,b;           /* Temporary variables.                            */
-  double hx,hy,
-    hxx,hyy,hxy;        /* The derivatives of the 1D surface, h(x,y).      */
-  double E,F,G;         /* The coefficents of the first fundamental form,
-			   that is, E = <Xu,Xu>, F = <Xu,Xv>  and
-			   G = <Xv,Xv>.                                    */
-  double e,f,g;         /* The coefficents of the second fundamental form,
-			   that is, e = <N,Xuu>, f = <N,Xuv> and
-			   g = <N,Xvv>.                                   */
+   double fundform[6]; /* The coefficients of the fundamental forms.
+			  The sequence is: E, F, G, e, f, g.         */
 
+   if (ider != 0) goto err178;
 
-  if (ider != 0) goto err178;
-
-  if (surf->idim == 1) /* 1D surface */
-  {
-    hx  = derive[1];
-    hy  = derive[2];
-    hxx = derive[3];
-    hxy = derive[4];
-    hyy = derive[5];
-
-    b = (1+hx*hx+hy*hy);
-    a = sqrt(b*b*b);
-
-    b = (1.0 + hx*hx)*hyy - 2.0*hx*hy*hxy + (1.0 + hy*hy)*hxx;
-
-    *meancurvature = 0.5*b/a;
-  }
+   if (surf->idim == 1 || surf->idim == 3) /* 1D and 3D surface */
+   {
+      s2513(surf, ider, 0, derive, normal, fundform, jstat);
+      if (*jstat < 0) goto error;
+      
+      *meancurvature = 0.5*(fundform[3]*fundform[2] - 2*fundform[4]*fundform[1] 
+			    + fundform[5]*fundform[0])
+	 /((fundform[0]*fundform[2] - fundform[1]*fundform[1])
+	   *sqrt(fundform[0]*fundform[2] - fundform[1]*fundform[1]));
+   }
 
   else if (surf->idim == 2) /* 2D surface */
   {
@@ -132,42 +120,12 @@ void s2503(SISLSurf *surf, int ider, double derive[], double normal[],
 
     *meancurvature = 0.0;
   }
-  else if (surf->idim == 3) /* 3D surface */
-  {
-    /* E = <Xu,Xu> */
-    E = derive[3]*derive[3]+derive[4]*derive[4]+derive[5]*derive[5];
-
-      /* F = <Xu,Xv> */
-    F = derive[3]*derive[6]+derive[4]*derive[7]+derive[5]*derive[8];
-
-    /* G = <Xv,Xv> */
-    G = derive[6]*derive[6]+derive[7]*derive[7]+derive[8]*derive[8];
-
-    /* b = EG + F^2. */
-    b = E*G-F*F;
-
-    /* e = <N,Xuu> (/ sqrt(E*G-F*F)) */
-    e = normal[0]*derive[9]+normal[1]*derive[10]+normal[2]*derive[11];
-
-    /* f = <N,Xuv> (/ sqrt(E*G-F*F)) */
-    f = normal[0]*derive[12]+normal[1]*derive[13]+normal[2]*derive[14];
-
-    /* g = <N,Xvv> (/ sqrt(E*G-F*F)) */
-    g = normal[0]*derive[15]+normal[1]*derive[16]+normal[2]*derive[17];
-
-    /* Compute mean curvature = 0.5(eG-2fF+gE)/(E*G-F*F). */
-    a = 0.5*(e*G - 2.0*f*F + g*E);
-    b = b*sqrt(b);
-    *meancurvature = a/b;
-  }
   else /* When surf->idim != 1,2 or 3 */
   {
     goto err105;
   }
 
-
-
-
+   
   /* Successful computations  */
 
   *jstat = 0;
@@ -183,6 +141,10 @@ err105:
   /* Illegal derivative requested. */
 err178:
   *jstat = -178;
+  s6err("s2503",*jstat,0);
+  goto out;
+  
+error:
   s6err("s2503",*jstat,0);
   goto out;
 
