ab1304a7f89521cd3c81c7a0a524c2c58b2ef732
jka <jka@sintef.no>
1999-11-19 09:03:13 +0000

    Initial revision

---
+++ src/s2550.c
@@ -0,0 +1,117 @@
+#include "copyright.h"
+
+
+#define S2550
+
+
+#include "sislP.h"
+
+#include "s2550.h"
+
+
+#if defined(SISLNEEDPROTOTYPES)
+void
+s2550( SISLCurve *curve,
+       double     ax[],
+       int        num_ax,
+       double     curvature[],
+       int       *jstat )
+#else
+void s2550( curve, ax, num_ax, curvature, jstat )
+     SISLCurve  *curve;
+     double      ax[];
+     int         num_ax;
+     double      curvature[];
+     int        *jstat;
+#endif
+/*
+*********************************************************************
+*
+*********************************************************************
+*
+* PURPOSE    : Evaluate the curvature of a curve at given parameter values
+*              ax[ 0 ],...,ax[ num_ax - 1 ].
+*
+*
+*
+* INPUT      : curve    - Pointer to the curve.
+*              ax       - The parameter values
+*          num_ax       - No. of parameter values
+*
+*
+*
+*
+* OUTPUT     :
+*              curvature - The "num_ax" curvature values computed
+*              jstat  - Status messages
+*                                         > 0      : Warning.
+*                                         = 0      : Ok.
+*                                         < 0      : Error.
+*
+*
+* METHOD     : Call the SISL rutine s2551() num_ax times
+*
+* REFERENCES :
+*
+*
+* CALLS      : s2551()
+*
+* WRITTEN BY  :   Geir Westgaard, SINTEF, Oslo, November 1999
+* MODIFIED BY :
+* REVISED BY  :
+*
+*********************************************************************
+*/
+{
+  int i;                      /* Index variable                      */
+  int kstat = 0;              /* local status variable               */
+  int kpos = 0;               /* local error position                */
+  int leftknot = 0;           /* Knot index                          */
+  double *derive = NULL;      /* Derivatives                         */
+
+
+
+  /* Allocate local arrays */
+
+  derive = newarray( 3*curve->idim, DOUBLE );
+
+  if ( derive == NULL )      goto err101;
+
+
+  /* Evaluate the curvature in all ax[i] position */
+
+  for ( i = 0; i < num_ax; i++ )
+  {
+    s2551( curve, ax[i], &leftknot, derive, curvature + i, &kstat );
+
+    if ( kstat < 0 ) goto error;
+  }
+
+
+
+
+ /* Successful computations.  */
+
+  *jstat = 0;
+  goto out;
+
+  /* Error in scratch allocation */
+ err101:
+  *jstat = -101;
+  s6err( "s2550",*jstat,kpos);
+
+  /* Error in lower level routine.  */
+
+ error:  *jstat = kstat;
+  s6err( "s2550", *jstat, kpos );
+  goto out;
+
+
+ out:
+  /* Free local arrays */
+
+ if ( derive != NULL ) freearray( derive );
+
+ return;
+
+}
---
+++ src/s2551.c
@@ -0,0 +1,155 @@
+#include "copyright.h"
+
+
+#define S2551
+
+
+#include "sislP.h"
+
+#include "s2551.h"
+
+
+#if defined(SISLNEEDPROTOTYPES)
+void
+s2551( SISLCurve *curve,
+       double     parvalue,
+       int       *leftknot,
+       double     derive[],
+       double    *curvature,
+       int       *jstat )
+#else
+void s2551( curve, parvalue, leftknot, derive, curvature, jstat )
+     SISLCurve  *curve;
+     double      parvalue;
+     int        *leftknot;
+     double      derive[];
+     double     *curvature;
+     int        *jstat;
+#endif
+/*
+*********************************************************************
+*
+*********************************************************************
+*
+* PURPOSE    : Evaluate the curvature of a curve at a given parameter value,
+*              from the right hand side.
+*
+*
+*
+* INPUT      : curve    - Pointer to the curve.
+*              parvalue - The parameter value at which to compute
+*                         curvature.
+*
+*
+*
+* INPUT/OUTPUT : leftknot - Pointer to the interval in the knot vector
+*                        where ax is located. If et is the knot vector,
+*                        the relation
+*
+*                          et[ileft] < parvalue <= et[ileft+1]
+*
+*                        should hold. (If parvalue == et[ik-1] then ileft
+*                        should be ik-1. Here in is the number of B-spline
+*                        coefficients.)
+*                        If ileft does not have the right value upon
+*                        entry to the routine, its value will be changed
+*                        to the value satisfying the above condition.
+*
+*
+*
+* OUTPUT     : derive   - Double array of dimension [(ider+1)*idim]
+*                       containing the position and derivative vectors.
+*                       (idim is the number of components of each B-spline
+*                       coefficient, i.e. the dimension of the Euclidean
+*                       space in which the curve lies.)
+*                       These vectors are stored in the following order:
+*                       First the idim components of the position vector,
+*                       then the idim components of the tangent vector,
+*                       then the idim components of the second derivative
+*                       vector, and so on.
+*                       (The C declaration of eder as a two dimensional array
+*                       would therefore be eder[ider+1,idim].)
+*              curvature - The curvature value computed
+*              jstat  - Status messages
+*                                         > 0      : Warning.
+*                                         = 0      : Ok.
+*                                         < 0      : Error.
+*
+*
+* METHOD     : The derivatives are evaluated from the right hand
+*              side by  s1221
+*              The curvature are evaluated by s1307
+*
+* REFERENCES :
+*
+*-
+* CALLS      : s1221, s1307
+*
+* WRITTEN BY  :   Geir Westgaard, SINTEF, Oslo, November 1999
+* MODIFIED BY :
+* REVISED BY  :
+*
+*********************************************************************
+*/
+{
+  int kdim = curve -> idim;   /* copy curve attribute to local parameter  */
+  int kstat = 0;              /* local status variable                    */
+  int kpos = 0;               /* local error position                     */
+  double *egeo = NULL;        /* pointer to store pos, tangent vector,
+				 curvature vector and radius of curvature */
+
+
+
+  /* Allocate local arrays */
+
+  egeo = newarray( 3*kdim+1, DOUBLE );
+
+  if ( egeo == NULL )      goto err101;
+
+
+  /* Evaluate the derivatives */
+
+  s1221( curve, 2, parvalue, leftknot, derive, &kstat );
+
+  if ( kstat < 0 ) goto error;
+
+
+  /* Evaluate the curvature vector and the radius_of_curvature */
+
+  s1307( derive, kdim, egeo, &kstat );
+
+  if ( kstat < 0 ) goto error;
+
+
+  /* Evaluate curvature */
+
+  *curvature = s6length( egeo+2*kdim, kdim, &kstat );
+
+
+
+
+ /* Successful computations.  */
+
+  *jstat = 0;
+  goto out;
+
+  /* Error in scratch allocation */
+ err101:
+  *jstat = -101;
+  s6err( "s2551",*jstat,kpos);
+
+  /* Error in lower level routine.  */
+
+ error:  *jstat = kstat;
+  s6err( "s2551", *jstat, kpos );
+  goto out;
+
+
+ out:
+  /* Free local arrays */
+
+ if ( egeo != NULL ) freearray( egeo );
+
+ return;
+
+}
